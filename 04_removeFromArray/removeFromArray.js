57b1b9a7da4d7d949c0fe8e161d016c38bbf47b8
GodKimba <rafaelfadel2@gmail.com>
2022-04-20 14:47:41 -0300

    stucked in the remove multiple itens exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, num) {
+    for (let i = 0; i <= array.length; i++) {
+        if (i == num) {
+            let index = array.indexOf(i)
+            array.splice(index, 1)
+        }
+    }
+   return array
 };
 
 // Do not edit below this line
