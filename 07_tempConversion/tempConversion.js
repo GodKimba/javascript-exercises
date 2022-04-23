7598a84cfa87c4ab31c49f80293aa92873b9e110
GodKimba <rafaelfadel2@gmail.com>
2022-04-23 17:20:55 -0300

    finished the 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperatureInFarenheit) {
+  temperatureInCelsius = ((temperatureInFarenheit - 32) * 5) / 9
+  return parseFloat(temperatureInCelsius.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureInCelsius) {
+  temperatureInFarenheit = ((temperatureInCelsius * 1.8) + 32)
+  return parseFloat(temperatureInFarenheit.toFixed(1))
 };
 
 // Do not edit below this line
